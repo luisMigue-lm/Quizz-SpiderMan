1ea5af9c79651b033ba32a37bea3b69aef7f96b1
luisMigue-lm <tavaresalvesluismiguel@gmail.com>
2024-12-12 17:40:28 -0300

    Finalizei a concepção de estilização do projeto e o JavaScript está totalmente funcional

--- script.js
+++ script.js
@@ -2,92 +2,92 @@ let contSpiderGwen = 0;
 let contSpiderPunk = 0;
 let contMilesMorales = 0;
 let contPorcoAranha = 0;
-let contAll = [Number]
+let contAll;
 let pag;
 
 let radios = document.querySelectorAll('input[type="radio"]');
 
 function calcularPontuacao() {
+  radios.forEach((resp) => {
+    if (resp.checked) {
+      if (resp.value == "punk-aranha") {
+        contSpiderPunk++;
+      } else if (resp.value == "spider-gwen") {
+        contSpiderGwen++;
+      } else if (resp.value == "miles-morales") {
+        contMilesMorales++;
+      } else if (resp.value == "porco-aranha") {
+        contPorcoAranha++;
+      }
+    }
+  });
 
-    radios.forEach(resp => {
+  contAll =
+    contMilesMorales + contPorcoAranha + contSpiderGwen + contSpiderPunk;
 
-        if (resp.checked) {
-
-            if (resp.value == "punk-aranha") {
-                contSpiderPunk++;
-
-            } else if (resp.value == "spider-gwen") {
-                contSpiderGwen++;
-
-            } else if (resp.value == "miles-morales") {
-                contMilesMorales++;
-
-            } else if (resp.value == "porco-aranha") {
-                contPorcoAranha++;
-
-            }
-
-        }
-
-    })
-
+  if (contAll == 10) {
     resultado();
     salvarDados();
-    maiorResultado();
-
+    temporizador();
+  
+    } else {
+        alert("Por favor, responda todas as 10 questões antes de continuar!");
+    }
 }
 
 function resultado() {
-    let resultado = `Miles Morales = ${contMilesMorales}<br>
-    Spider-Gwen = ${contSpiderGwen}<br>
-    Porco-Aranha = ${contPorcoAranha}<br>
-    Punk-Aranha = ${contSpiderPunk}`;
+  let maior = Math.max(
+    contMilesMorales,
+    contSpiderGwen,
+    contSpiderPunk,
+    contPorcoAranha
+  );
 
-    document.getElementById("resultado").innerHTML = resultado;
+  let empate = [];
+
+  if (contMilesMorales === maior) {
+    empate.push("miles-morales");
+  }
+
+  if (contSpiderGwen === maior) {
+    empate.push("spider-gwen");
+  }
+
+  if (contSpiderPunk === maior) {
+    empate.push("spider-punk");
+  }
+
+  if (contPorcoAranha === maior) {
+    empate.push("porco-aranha");
+  }
+
+  if (empate.length > 1) {
+    pag = "spider-man2099";
+
+  } else {
+    pag = empate[0];
+
+  }
+}
+
+function temporizador() {
+  let contador = 5;
+  const interval = setInterval(() => {
+    document.getElementById(
+      "resultado"
+    ).innerHTML = `Seu resultado sairá em: ${contador}`;
+    contador--;
+
+    if (contador < 0) {
+      clearInterval(interval);
+      window.open("/paginas/" + pag + ".html");
+    }
+  }, 1000);
 }
 
 function salvarDados() {
-    localStorage.setItem('punk-aranha', contSpiderPunk)
-    localStorage.setItem('spider-gwen', contSpiderGwen)
-    localStorage.setItem('miles-morales', contMilesMorales)
-    localStorage.setItem('porco-aranha', contPorcoAranha)
-}
-
-function maiorResultado() {
-    let maior = 0;
-
-
-    if (contMilesMorales >= maior) {
-        pag = "miles-morales"
-        maior = contMilesMorales
-
-    } 
-
-    if (contSpiderGwen >= maior) {
-        pag = "spider-gwen"
-        maior = contSpiderGwen
-    
-    } 
-
-    if (contSpiderPunk >= maior) {
-        pag = "spider-punk"
-        maior = contSpiderPunk
-
-    } 
-
-    if (contPorcoAranha >= maior) {
-        pag = "porco-aranha"
-        maior = contPorcoAranha
-
-    } 
-
-
-    setTimeout(() => {
-        window.open('/paginas/' +pag + '.html')
-    }, 5000);
-        
-    }
-
-function mudarPergunta() {
-    
+  localStorage.setItem("punk-aranha", contSpiderPunk);
+  localStorage.setItem("spider-gwen", contSpiderGwen);
+  localStorage.setItem("miles-morales", contMilesMorales);
+  localStorage.setItem("porco-aranha", contPorcoAranha);
 }
